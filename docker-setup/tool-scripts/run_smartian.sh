cb1eed7c06d436c64b222a889a889c8bafda945a
Fausto Carvalho <faustocarva@gmail.com>
2024-06-24 16:09:05 -0300

    making room for the new operation mode

--- docker-setup/tool-scripts/run_smartian.sh
+++ docker-setup/tool-scripts/run_smartian.sh
@@ -6,8 +6,9 @@
 # Arg4 : ABI file
 # Arg5 : Main contract name
 # Arg6 : Optional argument to pass
+# Arg7 : Seed dir
 
 mkdir -p /home/test/output
 dotnet /home/test/tools/Smartian/build/Smartian.dll fuzz \
-  --useothersoracle -t $1 -p $3 -a $4 -v 1 $6 -o /home/test/output \
+  --useothersoracle -t $1 -p $3 -a $4 -v 1 $6 -o /home/test/output -s $7 \
   > /home/test/output/log.txt 2>&1
