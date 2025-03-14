4ce2d5721d9b4766818c820392d3b728ad5b75ab
Fausto Carvalho <faustocarva@gmail.com>
2025-03-14 16:10:44 -0300

    new dataset b4 and scripts to run it

--- scripts/run_experiment.py
+++ scripts/run_experiment.py
@@ -2,8 +2,8 @@ import sys, os, subprocess, time
 from common import BASE_DIR, BENCHMARK_DIR
 
 IMAGE_NAME = "smartian-artifact"
-MAX_INSTANCE_NUM = 15
-AVAILABLE_BENCHMARKS = ["B1", "B1-noarg", "B2", "B3"]
+MAX_INSTANCE_NUM = 2
+AVAILABLE_BENCHMARKS = ["B1", "B1-noarg", "B2", "B3", "B4"]
 SUPPORTED_TOOLS = ["smartian", "sFuzz", "ilf", "mythril", "manticore"]
 
 def run_cmd(cmd_str):
