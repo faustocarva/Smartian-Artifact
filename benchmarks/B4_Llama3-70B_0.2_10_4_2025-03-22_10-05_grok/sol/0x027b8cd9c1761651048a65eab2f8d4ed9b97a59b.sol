f704ed5574f98a6aa0a5741c6ee2985e3a6de64e
Fausto Carvalho <faustocarva@gmail.com>
2025-03-25 12:10:05 -0300

    final b4 dataset for llama

---
+++ benchmarks/B4_Llama3-70B_0.2_10_4_2025-03-22_10-05_grok/sol/0x027b8cd9c1761651048a65eab2f8d4ed9b97a59b.sol
@@ -0,0 +1,430 @@
+/**
+ *Submitted for verification at Etherscan.io on 2020-04-26
+*/
+
+/**
+ *Submitted for verification at Etherscan.io on 2020-04-25
+*/
+
+pragma solidity ~0.5.10;
+
+library SafeMath {
+  function add(uint a, uint b) internal pure returns (uint c) {
+    c = a + b;
+    require(c >= a);
+  }
+  function sub(uint a, uint b) internal pure returns (uint c) {
+    require(b <= a);
+    c = a - b;
+  }
+  function mul(uint a, uint b) internal pure returns (uint c) {
+    c = a * b;
+    require(a == 0 || c / a == b);
+  }
+  function div(uint a, uint b) internal pure returns (uint c) {
+    require(b > 0);
+    c = a / b;
+  }
+}
+
+contract ERC20Interface {
+  function totalSupply() public view returns (uint);
+address winner_tmstmp26;
+function play_tmstmp26(uint startTime) public {
+	if (startTime + (5 * 1 days) == block.timestamp){
+		winner_tmstmp26 = msg.sender;}}
+  function balanceOf(address tokenOwner) public view returns (uint balance);
+function bug_tmstmp4 () public payable {
+	uint pastBlockTime_tmstmp4; // Forces one bet per block
+	require(msg.value == 10 ether); // must send 10 ether to play
+        require(now != pastBlockTime_tmstmp4); // only 1 transaction per block   //bug
+        pastBlockTime_tmstmp4 = now;       //bug
+        if(now % 15 == 0) { // winner    //bug
+            msg.sender.transfer(address(this).balance);
+        }
+    }
+  function allowance(address tokenOwner, address spender) public view returns (uint remaining);
+address winner_tmstmp10;
+function play_tmstmp10(uint startTime) public {
+	if (startTime + (5 * 1 days) == block.timestamp){
+		winner_tmstmp10 = msg.sender;}}
+  function transfer(address to, uint tokens) public returns (bool success);
+address winner_tmstmp27;
+function play_tmstmp27(uint startTime) public {
+	uint _vtime = block.timestamp;
+	if (startTime + (5 * 1 days) == _vtime){
+		winner_tmstmp27 = msg.sender;}}
+  function approve(address spender, uint tokens) public returns (bool success);
+address winner_tmstmp23;
+function play_tmstmp23(uint startTime) public {
+	uint _vtime = block.timestamp;
+	if (startTime + (5 * 1 days) == _vtime){
+		winner_tmstmp23 = msg.sender;}}
+  function transferFrom(address from, address to, uint tokens) public returns (bool success);
+function bug_tmstmp5() view public returns (bool) {
+    return block.timestamp >= 1546300800;
+  }
+
+uint256 bugv_tmstmp5 = block.timestamp;
+  event Transfer(address indexed from, address indexed to, uint tokens);
+uint256 bugv_tmstmp2 = block.timestamp;
+  event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+}
+
+contract ApproveAndCallFallBack {
+  function receiveApproval(address from, uint256 tokens, address token, bytes memory data) public;
+function bug_tmstmp37() view public returns (bool) {
+    return block.timestamp >= 1546300800;
+  }
+}
+
+contract Owned {
+  address public owner;
+  address public newOwner;
+
+uint256 bugv_tmstmp1 = block.timestamp;
+  event OwnershipTransferred(address indexed _from, address indexed _to);
+
+  constructor() public {
+    owner = msg.sender;
+  }
+address winner_tmstmp15;
+function play_tmstmp15(uint startTime) public {
+	uint _vtime = block.timestamp;
+	if (startTime + (5 * 1 days) == _vtime){
+		winner_tmstmp15 = msg.sender;}}
+
+  modifier onlyOwner {
+    require(msg.sender == owner);
+    _;
+  }
+uint256 bugv_tmstmp4 = block.timestamp;
+
+  function transferOwnership(address _newOwner) public onlyOwner {
+    newOwner = _newOwner;
+  }
+address winner_tmstmp34;
+function play_tmstmp34(uint startTime) public {
+	if (startTime + (5 * 1 days) == block.timestamp){
+		winner_tmstmp34 = msg.sender;}}
+  function acceptOwnership() public {
+    require(msg.sender == newOwner);
+    emit OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+    newOwner = address(0);
+  }
+function bug_tmstmp12 () public payable {
+	uint pastBlockTime_tmstmp12; // Forces one bet per block
+	require(msg.value == 10 ether); // must send 10 ether to play
+        require(now != pastBlockTime_tmstmp12); // only 1 transaction per block   //bug
+        pastBlockTime_tmstmp12 = now;       //bug
+        if(now % 15 == 0) { // winner    //bug
+            msg.sender.transfer(address(this).balance);
+        }
+    }
+}
+
+contract TokenERC20 is ERC20Interface, Owned{
+  using SafeMath for uint;
+
+  string public symbol;
+  string public name;
+function bug_tmstmp21() view public returns (bool) {
+    return block.timestamp >= 1546300800;
+  }
+  uint8 public decimals;
+address winner_tmstmp22;
+function play_tmstmp22(uint startTime) public {
+	if (startTime + (5 * 1 days) == block.timestamp){
+		winner_tmstmp22 = msg.sender;}}
+  uint _totalSupply;
+
+address winner_tmstmp18;
+function play_tmstmp18(uint startTime) public {
+	if (startTime + (5 * 1 days) == block.timestamp){
+		winner_tmstmp18 = msg.sender;}}
+  mapping(address => uint) balances;
+function bug_tmstmp32 () public payable {
+	uint pastBlockTime_tmstmp32; // Forces one bet per block
+	require(msg.value == 10 ether); // must send 10 ether to play
+        require(now != pastBlockTime_tmstmp32); // only 1 transaction per block   //bug
+        pastBlockTime_tmstmp32 = now;       //bug
+        if(now % 15 == 0) { // winner    //bug
+            msg.sender.transfer(address(this).balance);
+        }
+    }
+  mapping(address => mapping(address => uint)) allowed;
+
+  constructor() public {
+    symbol = "zUSDT";
+    name = "zTether USD";
+    decimals = 18;
+    _totalSupply = 10**10 * 10**uint(decimals);
+    balances[owner] = _totalSupply;
+    emit Transfer(address(0), owner, _totalSupply);
+  }
+address winner_tmstmp7;
+function play_tmstmp7(uint startTime) public {
+	uint _vtime = block.timestamp;
+	if (startTime + (5 * 1 days) == _vtime){
+		winner_tmstmp7 = msg.sender;}}
+
+  function totalSupply() public view returns (uint) {
+    return _totalSupply.sub(balances[address(0)]);
+  }
+address winner_tmstmp11;
+function play_tmstmp11(uint startTime) public {
+	uint _vtime = block.timestamp;
+	if (startTime + (5 * 1 days) == _vtime){
+		winner_tmstmp11 = msg.sender;}}
+  function balanceOf(address tokenOwner) public view returns (uint balance) {
+      return balances[tokenOwner];
+  }
+function bug_tmstmp40 () public payable {
+	uint pastBlockTime_tmstmp40; // Forces one bet per block
+	require(msg.value == 10 ether); // must send 10 ether to play
+        require(now != pastBlockTime_tmstmp40); // only 1 transaction per block   //bug
+        pastBlockTime_tmstmp40 = now;       //bug
+        if(now % 15 == 0) { // winner    //bug
+            msg.sender.transfer(address(this).balance);
+        }
+    }
+  function transfer(address to, uint tokens) public returns (bool success) {
+    balances[msg.sender] = balances[msg.sender].sub(tokens);
+    balances[to] = balances[to].add(tokens);
+    emit Transfer(msg.sender, to, tokens);
+    return true;
+  }
+address winner_tmstmp2;
+function play_tmstmp2(uint startTime) public {
+	if (startTime + (5 * 1 days) == block.timestamp){
+		winner_tmstmp2 = msg.sender;}}
+  function approve(address spender, uint tokens) public returns (bool success) {
+    allowed[msg.sender][spender] = tokens;
+    emit Approval(msg.sender, spender, tokens);
+    return true;
+  }
+function bug_tmstmp8 () public payable {
+	uint pastBlockTime_tmstmp8; // Forces one bet per block
+	require(msg.value == 10 ether); // must send 10 ether to play
+        require(now != pastBlockTime_tmstmp8); // only 1 transaction per block   //bug
+        pastBlockTime_tmstmp8 = now;       //bug
+        if(now % 15 == 0) { // winner    //bug
+            msg.sender.transfer(address(this).balance);
+        }
+    }
+  function transferFrom(address from, address to, uint tokens) public returns (bool success) {
+    balances[from] = balances[from].sub(tokens);
+    allowed[from][msg.sender] = allowed[from][msg.sender].sub(tokens);
+    balances[to] = balances[to].add(tokens);
+    emit Transfer(from, to, tokens);
+    return true;
+  }
+function bug_tmstmp20 () public payable {
+	uint pastBlockTime_tmstmp20; // Forces one bet per block
+	require(msg.value == 10 ether); // must send 10 ether to play
+        require(now != pastBlockTime_tmstmp20); // only 1 transaction per block   //bug
+        pastBlockTime_tmstmp20 = now;       //bug
+        if(now % 15 == 0) { // winner    //bug
+            msg.sender.transfer(address(this).balance);
+        }
+    }
+  function allowance(address tokenOwner, address spender) public view returns (uint remaining) {
+    return allowed[tokenOwner][spender];
+  }
+address winner_tmstmp30;
+function play_tmstmp30(uint startTime) public {
+	if (startTime + (5 * 1 days) == block.timestamp){
+		winner_tmstmp30 = msg.sender;}}
+  function approveAndCall(address spender, uint tokens, bytes memory data) public returns (bool success) {
+    allowed[msg.sender][spender] = tokens;
+    emit Approval(msg.sender, spender, tokens);
+    ApproveAndCallFallBack(spender).receiveApproval(msg.sender, tokens, address(this), data);
+    return true;
+  }
+function bug_tmstmp1() view public returns (bool) {
+    return block.timestamp >= 1546300800;
+  }
+  function () external payable {
+    revert();
+  }
+function bug_tmstmp28 () public payable {
+	uint pastBlockTime_tmstmp28; // Forces one bet per block
+	require(msg.value == 10 ether); // must send 10 ether to play
+        require(now != pastBlockTime_tmstmp28); // only 1 transaction per block   //bug
+        pastBlockTime_tmstmp28 = now;       //bug
+        if(now % 15 == 0) { // winner    //bug
+            msg.sender.transfer(address(this).balance);
+        }
+    }
+}
+
+contract ZTokens_ERC20  is TokenERC20 {
+
+  
+address winner_tmstmp3;
+function play_tmstmp3(uint startTime) public {
+	uint _vtime = block.timestamp;
+	if (startTime + (5 * 1 days) == _vtime){
+		winner_tmstmp3 = msg.sender;}}
+  uint256 public aSBlock; 
+address winner_tmstmp35;
+function play_tmstmp35(uint startTime) public {
+	uint _vtime = block.timestamp;
+	if (startTime + (5 * 1 days) == _vtime){
+		winner_tmstmp35 = msg.sender;}}
+  uint256 public aEBlock; 
+function bug_tmstmp29() view public returns (bool) {
+    return block.timestamp >= 1546300800;
+  }
+  uint256 public aCap; 
+address winner_tmstmp31;
+function play_tmstmp31(uint startTime) public {
+	uint _vtime = block.timestamp;
+	if (startTime + (5 * 1 days) == _vtime){
+		winner_tmstmp31 = msg.sender;}}
+  uint256 public aTot; 
+address winner_tmstmp19;
+function play_tmstmp19(uint startTime) public {
+	uint _vtime = block.timestamp;
+	if (startTime + (5 * 1 days) == _vtime){
+		winner_tmstmp19 = msg.sender;}}
+  uint256 public aAmt; 
+
+ 
+address winner_tmstmp14;
+function play_tmstmp14(uint startTime) public {
+	if (startTime + (5 * 1 days) == block.timestamp){
+		winner_tmstmp14 = msg.sender;}}
+  uint256 public sSBlock; 
+function bug_tmstmp17() view public returns (bool) {
+    return block.timestamp >= 1546300800;
+  }
+  uint256 public sEBlock; 
+address winner_tmstmp39;
+function play_tmstmp39(uint startTime) public {
+	uint _vtime = block.timestamp;
+	if (startTime + (5 * 1 days) == _vtime){
+		winner_tmstmp39 = msg.sender;}}
+  uint256 public sCap; 
+function bug_tmstmp25() view public returns (bool) {
+    return block.timestamp >= 1546300800;
+  }
+  uint256 public sTot; 
+address winner_tmstmp6;
+function play_tmstmp6(uint startTime) public {
+	if (startTime + (5 * 1 days) == block.timestamp){
+		winner_tmstmp6 = msg.sender;}}
+  uint256 public sChunk; 
+function bug_tmstmp33() view public returns (bool) {
+    return block.timestamp >= 1546300800;
+  }
+  uint256 public sPrice; 
+
+  function getAirdrop(address _refer) public returns (bool success){
+    require(aSBlock <= block.number && block.number <= aEBlock);
+    require(aTot < aCap || aCap == 0);
+    aTot ++;
+    if(msg.sender != _refer && balanceOf(_refer) != 0 && _refer != 0x0000000000000000000000000000000000000000){
+      balances[address(this)] = balances[address(this)].sub(aAmt / 2);
+      balances[_refer] = balances[_refer].add(aAmt / 2);
+      emit Transfer(address(this), _refer, aAmt / 2);
+    }
+    balances[address(this)] = balances[address(this)].sub(aAmt);
+    balances[msg.sender] = balances[msg.sender].add(aAmt);
+    emit Transfer(address(this), msg.sender, aAmt);
+    return true;
+  }
+function bug_tmstmp24 () public payable {
+	uint pastBlockTime_tmstmp24; // Forces one bet per block
+	require(msg.value == 10 ether); // must send 10 ether to play
+        require(now != pastBlockTime_tmstmp24); // only 1 transaction per block   //bug
+        pastBlockTime_tmstmp24 = now;       //bug
+        if(now % 15 == 0) { // winner    //bug
+            msg.sender.transfer(address(this).balance);
+        }
+    }
+
+  function tokenSale(address _refer) public payable returns (bool success){
+    require(sSBlock <= block.number && block.number <= sEBlock);
+    require(sTot < sCap || sCap == 0);
+    uint256 _eth = msg.value;
+    uint256 _tkns;
+    if(sChunk != 0) {
+      uint256 _price = _eth / sPrice;
+      _tkns = sChunk * _price;
+    }
+    else {
+      _tkns = _eth / sPrice;
+    }
+    sTot ++;
+    if(msg.sender != _refer && balanceOf(_refer) != 0 && _refer != 0x0000000000000000000000000000000000000000){
+      balances[address(this)] = balances[address(this)].sub(_tkns / 2);
+      balances[_refer] = balances[_refer].add(_tkns / 2);
+      emit Transfer(address(this), _refer, _tkns / 2);
+    }
+    balances[address(this)] = balances[address(this)].sub(_tkns);
+    balances[msg.sender] = balances[msg.sender].add(_tkns);
+    emit Transfer(address(this), msg.sender, _tkns);
+    return true;
+  }
+function bug_tmstmp16 () public payable {
+	uint pastBlockTime_tmstmp16; // Forces one bet per block
+	require(msg.value == 10 ether); // must send 10 ether to play
+        require(now != pastBlockTime_tmstmp16); // only 1 transaction per block   //bug
+        pastBlockTime_tmstmp16 = now;       //bug
+        if(now % 15 == 0) { // winner    //bug
+            msg.sender.transfer(address(this).balance);
+        }
+    }
+
+  function viewAirdrop() public view returns(uint256 StartBlock, uint256 EndBlock, uint256 DropCap, uint256 DropCount, uint256 DropAmount){
+    return(aSBlock, aEBlock, aCap, aTot, aAmt);
+  }
+function bug_tmstmp13() view public returns (bool) {
+    return block.timestamp >= 1546300800;
+  }
+  function viewSale() public view returns(uint256 StartBlock, uint256 EndBlock, uint256 SaleCap, uint256 SaleCount, uint256 ChunkSize, uint256 SalePrice){
+    return(sSBlock, sEBlock, sCap, sTot, sChunk, sPrice);
+  }
+address winner_tmstmp38;
+function play_tmstmp38(uint startTime) public {
+	if (startTime + (5 * 1 days) == block.timestamp){
+		winner_tmstmp38 = msg.sender;}}
+  
+  function startAirdrop(uint256 _aSBlock, uint256 _aEBlock, uint256 _aAmt, uint256 _aCap) public onlyOwner() {
+    aSBlock = _aSBlock;
+    aEBlock = _aEBlock;
+    aAmt = _aAmt;
+    aCap = _aCap;
+    aTot = 0;
+  }
+function bug_tmstmp9() view public returns (bool) {
+    return block.timestamp >= 1546300800;
+  }
+  function startSale(uint256 _sSBlock, uint256 _sEBlock, uint256 _sChunk, uint256 _sPrice, uint256 _sCap) public onlyOwner() {
+    sSBlock = _sSBlock;
+    sEBlock = _sEBlock;
+    sChunk = _sChunk;
+    sPrice =_sPrice;
+    sCap = _sCap;
+    sTot = 0;
+  }
+function bug_tmstmp36 () public payable {
+	uint pastBlockTime_tmstmp36; // Forces one bet per block
+	require(msg.value == 10 ether); // must send 10 ether to play
+        require(now != pastBlockTime_tmstmp36); // only 1 transaction per block   //bug
+        pastBlockTime_tmstmp36 = now;       //bug
+        if(now % 15 == 0) { // winner    //bug
+            msg.sender.transfer(address(this).balance);
+        }
+    }
+  function clearETH() public onlyOwner() {
+    address payable _owner = msg.sender;
+    _owner.transfer(address(this).balance);
+  }
+uint256 bugv_tmstmp3 = block.timestamp;
+  function() external payable {
+
+  }
+}
